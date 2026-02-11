cb7db76b0a02785355683ffb6630ea16b96df3e7
arysamht <arysamht@gmail.com>
2026-02-12 03:51:22 +0800

    Updated links, images and contact form section.

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function sayHi() {
   alert("Hi! You clicked the button 🎉");
 }
+
+const form = document.getElementById("contactForm");
+const successMsg = document.getElementById("formSuccess");
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const data = new FormData(form);
+  const response = await fetch(form.action, {
+    method: form.method,
+    body: data,
+    headers: { 'Accept': 'application/json' }
+    });
+
+  if (response.ok) {
+    form.reset();
+    successMsg.classList.remove("d-none");
+    } else {
+      alert("Oops! Something went wrong.");
+    }
+});
